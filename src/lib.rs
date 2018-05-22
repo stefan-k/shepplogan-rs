4800b715e27d516f7a39846d7ada8ea1d4512789
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-05-22 17:16:55 +0200

    much more documentation

--- src/lib.rs
+++ src/lib.rs
@@ -14,22 +14,75 @@
 //! This crate provides an efficient implementation for creating Shepp-Logan phantoms in 2D. 
 //! The following results were obtained with `cargo bench` on an Intel Core i7 with 2.70GHz:
 //!
-//! | Resolution | time        | fps  |
-//! | -----------|------------:|-----:|
-//! | 128x128    |   111,000ns | 9000 |
-//! | 256x256    |   440,000ns | 2200 |
-//! | 512x512    | 1,780,000ns |  560 |
+//! Resolution | time        | fps   
+//! -----------|-------------|------ 
+//! 128x128    |   111,000ns | 9000  
+//! 256x256    |   440,000ns | 2200  
+//! 512x512    | 1,780,000ns |  560  
 //!
-//! EXAMPLES
+//! Two versions are provided: The original version as described in [0] and a modified version,
+//! which has higher contrast as described in [1]. If you do not know the difference between those
+//! two, you most likely want the modified version.
 //!
-//! REFERENCES
+//! To use the crate, add `shepplogan` to your `Cargo.toml`:
+//!
+//! ```toml
+//! shepplogan = "^1"
+//! ```
+//! 
+//! # Example
+//!
+//! ```rust
+//! extern crate shepplogan;
+//! use shepplogan::{shepplogan, shepplogan_modified};
+//!
+//! // Dimensions of the image grid
+//! let (nx, ny) = (256, 320);
+//!
+//! // Original Shepp-Logan Phantom (the dynamic range is between 0.0 and 2.0)
+//! let phantom = shepplogan(nx, ny);
+//!
+//! // Modified Shepp-Logan Phantom (the dynamic range is between 0.0 and 1.0)
+//! let phantom_modified = shepplogan_modified(nx, ny);
+//! ```
+//!
+//! See `examples/example.rs` for an example which saves the phantom to disk.
+//!
+//! You can also create your own phantom by defining ellipses:
+//!
+//! ```rust
+//! extern crate shepplogan;
+//! use shepplogan::{phantom, Ellipse};
+//!
+//! // Dimensions of the image grid
+//! let (nx, ny) = (256, 320);
+//!
+//! // Define two ellipses
+//! let ellipses = 
+//!     [
+//!         Ellipse::new(0.0, -0.0184, 0.6624, 0.874, 0.0, -0.98),
+//!         Ellipse::new(0.0, 0.0, 0.69, 0.92, 0.0, 2.0),
+//!     ];
+//!
+//! let ph = phantom(&ellipses, nx, ny);
+//! ```
+//! 
+//! This will create a phantom consisting of two ellipses.
+//!
+//! # References
+//!
+//! [0] Shepp, LA and Logan BF, "The Fourier reconstruction of a head section." IEEE Transactions
+//! on Nuclear Science 21, No. 3 (1974)
+//!
+//! [1] Toft, PA, "The Radon Transform - Theory and Implementation", PhD dissertation, Departement
+//! of Mathematical Modelling, Technical University of Denmark (1996)
 
 #![warn(missing_docs)]
 #[cfg(feature = "parallel")]
 extern crate rayon;
 
 mod ellipse;
-use ellipse::Ellipse;
+pub use ellipse::Ellipse;
 #[cfg(feature = "parallel")]
 use rayon::prelude::*;
 #[cfg(feature = "parallel")]
@@ -70,12 +123,28 @@ macro_rules! parts_modified {
 }
 
 /// Original Shepp-Logan phantom
+///
+/// Constructs the original Shepp-Logan phantom as described in:
+///
+/// Shepp, LA and Logan BF, "The Fourier reconstruction of a head section." IEEE Transactions on
+/// Nuclear Science 21, No. 3 (1974)
+///
+/// The parameters `nx` and `ny` define the number of pixels in `x` and `y` direction.
+/// The dynamic range of the values is between `0.0` and `2.0`.
 pub fn shepplogan(nx: usize, ny: usize) -> Vec<f64> {
     let ellipses = parts!();
     phantom(&ellipses, nx, ny)
 }
 
 /// Modified Shepp-Logan phantom with better contrast
+///
+/// Constructs the modified Shepp-Logan phantom as described in:
+///
+/// Toft, PA, "The Radon Transform - Theory and Implementation", PhD dissertation, Departement of
+/// Mathematical Modelling, Technical University of Denmark (1996)
+///
+/// The parameters `nx` and `ny` define the number of pixels in `x` and `y` direction.
+/// The dynamic range of the values is between `0.0` and `1.0`.
 pub fn shepplogan_modified(nx: usize, ny: usize) -> Vec<f64> {
     let ellipses = parts_modified!();
     phantom(&ellipses, nx, ny)
@@ -83,7 +152,11 @@ pub fn shepplogan_modified(nx: usize, ny: usize) -> Vec<f64> {
 
 #[cfg(not(feature = "parallel"))]
 #[cfg(not(feature = "slow_impl"))]
-fn phantom(ellipses: &[Ellipse], nx: usize, ny: usize) -> Vec<f64> {
+/// Creates a phantom based on given ellipses
+///
+/// Besides `nx` and `ny`, which define the number of pixels in `x` and `y` direction, this
+/// function also requires a vector of Ellipses.
+pub fn phantom(ellipses: &[Ellipse], nx: usize, ny: usize) -> Vec<f64> {
     let mut arr = vec![0.0; nx * ny];
     let nx2 = (nx as f64) / 2.0;
     let ny2 = (ny as f64) / 2.0;
@@ -105,7 +178,7 @@ fn phantom(ellipses: &[Ellipse], nx: usize, ny: usize) -> Vec<f64> {
 }
 
 #[cfg(feature = "parallel")]
-fn phantom(ellipses: &[Ellipse], nx: usize, ny: usize) -> Vec<f64> {
+pub fn phantom(ellipses: &[Ellipse], nx: usize, ny: usize) -> Vec<f64> {
     let arr: Vec<Mutex<f64>> = (0..(nx * ny))
         .into_par_iter()
         .map(|_| Mutex::new(0.0))
@@ -131,7 +204,7 @@ fn phantom(ellipses: &[Ellipse], nx: usize, ny: usize) -> Vec<f64> {
 }
 
 #[cfg(feature = "slow_impl")]
-fn phantom(ellipses: &[Ellipse], nx: usize, ny: usize) -> Vec<f64> {
+pub fn phantom(ellipses: &[Ellipse], nx: usize, ny: usize) -> Vec<f64> {
     let mut arr = Vec::with_capacity(nx * ny);
     let nx2 = (nx as f64) / 2.0;
     let ny2 = (ny as f64) / 2.0;
