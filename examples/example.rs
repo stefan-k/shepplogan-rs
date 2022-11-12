51638335e78f97b39529e36e5b2d6458d1dab381
Stefan Kroboth <stefan.kroboth@gmail.com>
2022-11-12 16:41:50 +0100

    Initial design of new API

--- examples/example.rs
+++ examples/example.rs
@@ -5,8 +5,7 @@
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
 
-extern crate image;
-extern crate shepplogan;
+use shepplogan::{Phantom, SheppLogan};
 
 fn main() {
     // let nx = 256;
@@ -17,17 +16,17 @@ fn main() {
     // let ny = 1024;
 
     // // Original Shepp-Logan phantom
-    // let phantom = shepplogan::shepplogan(nx, ny);
+    let phantom = shepplogan::shepplogan(nx, ny);
 
-    // let phantom: Vec<u8> = phantom.iter().map(|x| (*x / 2.0 * 255.0) as u8).collect();
-    // image::save_buffer(
-    //     "shepp_logan.png",
-    //     &phantom,
-    //     nx as u32,
-    //     ny as u32,
-    //     image::ColorType::L8,
-    // )
-    // .unwrap();
+    let phantom: Vec<u8> = phantom.iter().map(|x| (*x / 2.0 * 255.0) as u8).collect();
+    image::save_buffer(
+        "shepp_logan.png",
+        &phantom,
+        nx as u32,
+        ny as u32,
+        image::ColorType::L8,
+    )
+    .unwrap();
 
     // Modified Shepp-Logan phantom
     let phantom = shepplogan::shepplogan_modified(nx, ny);
@@ -35,6 +34,16 @@ fn main() {
     let phantom: Vec<u8> = phantom.iter().map(|x| (*x * 255.0) as u8).collect();
     image::save_buffer(
         "shepp_logan_modified.png",
+        &phantom,
+        nx as u32,
+        ny as u32,
+        image::ColorType::L8,
+    )
+    .unwrap();
+
+    let phantom: Vec<u8> = SheppLogan::new(nx, ny, true).scale(255.0).into_vec();
+    image::save_buffer(
+        "shepp_logan_modified_new.png",
         &phantom,
         nx as u32,
         ny as u32,
